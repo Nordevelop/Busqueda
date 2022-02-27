f0d5f7e5d949f4ac9007cae002cf9abeb128438f
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-02-26 22:12:03 -0300

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -18,8 +18,8 @@ public class BinarySearch {
     static int search(int[] nums, int target) {  //metodo Busqueda
         int primero = 0;
         int ultimo = nums.length - 1;
-
-        while (primero <= ultimo) {  
+                                                                                                                 //x                                                                         
+        while (primero <= ultimo) {  //Mientras primero se mayor o igual al ultimo elemento del Array |_|_|_|_|_|_|_|
             int mid = primero + (ultimo - primero) / 2;
 
             if (nums[mid] > target)         
